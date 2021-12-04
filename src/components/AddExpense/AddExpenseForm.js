f4d3a65b662570b738ccba84f85c88224623ca1a
amiedawn <amie.d.carroll@gmail.com>
2021-12-03 21:04:16 -0700

    formatted addexpense form

--- src/components/AddExpense/AddExpenseForm.js
+++ src/components/AddExpense/AddExpenseForm.js
@@ -1,31 +1,53 @@
-import React from 'react';
-import './AddExpenseStyle.css';
+import React from "react";
+import "./AddExpenseStyle.css";
 
-const AddExpenseForm = ({ name, amount, handleName, handleAmount, handleSubmit }) => {
+const AddExpenseForm = ({
+  name,
+  amount,
+  handleName,
+  handleAmount,
+  handleSubmit,
+}) => {
   return (
     <form className="add-expense-container" onSubmit={handleSubmit}>
       <h2 className="add-expense-title">Add Expense</h2>
-      
-        <div className="form-layout">
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" id="name" name="name" 
-            value={name} onChange={handleName}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="amount">Cost</label>
-            <input type="text" className="form-control" name="amount" id="amount"
-            value={amount} onChange={handleAmount}
-            />
-          </div>
-        </div>
-        <div>
-          <button type="submit" className="btn-save">Save</button>
-        </div>
-     
+      <tr>
+        <th className="expense-header">
+          <label htmlFor="name">Name</label>
+        </th>
+        <th className="expense-header">
+          <label htmlFor="amount">Cost</label>
+        </th>
+      </tr>
+      <tr>
+        <td>
+          {" "}
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleName}
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            className="form-control"
+            name="amount"
+            id="amount"
+            value={amount}
+            onChange={handleAmount}
+          />
+        </td>
+      </tr>
+
+      <button type="submit" className="btn-save">
+        Save
+      </button>
     </form>
   );
-}
+};
 
 export default AddExpenseForm;
