8644053bb9f9690539dff8b55dac7e55fc280924
amiedawn <amie.d.carroll@gmail.com>
2021-12-05 22:41:42 -0700

    budget is editable

--- src/components/Totals/Totals.js
+++ src/components/Totals/Totals.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import "./TotalsStyle.css";
 
 const Totals = ({ expenses }) => {
-  let budget = 2000;
-  
+
+  const [budget, setBudget] = useState(2000);
+
   let totalSpent = 
     expenses.reduce((tot, curr) => {
       return (tot += parseInt(curr.amount));
     }, 0);
-  
- 
+   
   let remaining = budget - totalSpent;
 
   return (
@@ -17,7 +17,9 @@ const Totals = ({ expenses }) => {
       <h1>My Budget Planner</h1>
       <div className="wrapper">
         <div className="budget-container">
-          <h3 className="budget-title">Budget: ${budget}</h3>
+          <label htmlFor="budget">Budget: $</label>
+          <input type="text" className="budget-title" name="budget" value={budget} 
+          onChange={(e) => setBudget(e.target.value)} />
           <button className="budget-edit">Edit</button>
         </div>
         <div className="remaining-container">
